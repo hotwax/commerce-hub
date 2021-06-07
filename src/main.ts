1587c12f6511d019e57c9650e5c7fd47cc6add81
Aditya Sharma <aditya.sharma@hotwaxsystems.com>
2021-06-07 15:01:46 +0530

    Initial Ionic Vue App Commit

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,32 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { IonicVue } from '@ionic/vue';
 
-if (environment.production) {
-  enableProdMode();
-}
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/vue/css/core.css';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+/* Basic CSS for apps built with Ionic */
+import '@ionic/vue/css/normalize.css';
+import '@ionic/vue/css/structure.css';
+import '@ionic/vue/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/vue/css/padding.css';
+import '@ionic/vue/css/float-elements.css';
+import '@ionic/vue/css/text-alignment.css';
+import '@ionic/vue/css/text-transformation.css';
+import '@ionic/vue/css/flex-utils.css';
+import '@ionic/vue/css/display.css';
+
+/* Theme variables */
+import './theme/variables.css';
+
+const app = createApp(App)
+  .use(IonicVue)
+  .use(router);
+  
+router.isReady().then(() => {
+  app.mount('#app');
+});
--- src/models/auth.model.ts
+++
@@ -1,3 +0,0 @@
-export interface AuthStateModel {
-  token: string | null;
-}
