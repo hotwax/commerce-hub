31a0e42838d2195469313f38892aacd56d7be021
Naman-HotWax <naman.garg@hotwaxsystems.com>
2021-08-05 20:52:14 +0530

    Improved: code to use radio-button in the Settings page(#51fwvt)

--- src/views/Store-Popover.vue
+++ src/views/Store-Popover.vue
@@ -1,20 +1,30 @@
 <template>
   <ion-content class="ion-padding">
     <ion-list>
-        <ion-item>Store 1</ion-item>
-        <ion-item>Store 2</ion-item>
-        <ion-item>Store 3</ion-item>
-        <ion-item lines="none">Store 4</ion-item>
+        <ion-radio-group value="rd" >
+        <ion-item >
+          <ion-label>Store 1</ion-label>
+          <ion-radio value="store 1" slot="start"></ion-radio>
+        </ion-item>
+        <ion-item >
+          <ion-label>Store 2</ion-label>
+          <ion-radio value="store 2" slot="start"></ion-radio>
+        </ion-item>
+        <ion-item >
+          <ion-label>Store 3</ion-label>
+          <ion-radio value="store 3" slot="start"></ion-radio>
+        </ion-item>
+      </ion-radio-group>
     </ion-list>
   </ion-content>
 </template>
 
 <script>
-import { IonContent, IonItem, IonList } from '@ionic/vue';
+import { IonContent, IonList, IonRadioGroup, IonItem, IonLabel, IonRadio } from '@ionic/vue';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'Store-Popover',
-  components: { IonContent, IonItem, IonList }
+  components: { IonContent, IonItem, IonList, IonRadioGroup, IonLabel, IonRadio }
 });
 </script>
