bf5172ebfc58ef471dc349d5845dc18c381254de
Aniket Kumar Pandey <meet.aniket.kumar@gmail.com>
2021-12-17 18:23:35 +0530

    Improved code quality as per JS standered (#1wn444v)

--- src/store/modules/order/mutations.ts
+++ src/store/modules/order/mutations.ts
@@ -4,7 +4,6 @@ import * as types from './mutation-types'
 
 const mutations: MutationTree <OrderState> = {
     [types.ORDER_LIST_UPDATED] (state, payload) {
-        console.log(payload)
         state.list.items = payload.items
     }
 }
