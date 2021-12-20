24485f971a29f2535fe779f3f7e26e541b35caa3
Aniket Kumar Pandey <meet.aniket.kumar@gmail.com>
2021-12-20 17:46:49 +0530

    Deleted import statement for ProductInventory page which was causing error (#1wn444v)

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
 import Home from '@/views/Home.vue'
 import Order from '@/views/Orders.vue'
-// import ProductInventory from '@/views/ProductInventory.vue'
 import Login from '@/views/Login.vue'
 import Settings from "@/views/Settings.vue"
 import store from '@/store'
