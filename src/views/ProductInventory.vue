0b95efe77f815b5d11352b3c807965b78b18900a
Azkya Khan <azkya.khan@hotwax.co>
2021-12-21 19:42:52 +0530

    Refactored markup and styling of product inventory page (#1x0kmtf)

--- src/views/ProductInventory.vue
+++ src/views/ProductInventory.vue
@@ -14,8 +14,218 @@
         </ion-buttons>
       </ion-toolbar>
     </ion-header>
+
     <ion-content>
-      <div class="product-find">
+      <div class="find">
+        <section class="search">
+          <ion-searchbar />
+        </section>
+     
+        <aside class="filters desktop-only">
+          <ion-list>
+            <ion-list-header>{{ $t("Catalog") }}</ion-list-header>
+              <ion-item>
+                <ion-label>{{ $t("Categories") }}</ion-label>
+                <ion-select value="any">
+                  <ion-select-option value="any">all</ion-select-option>
+                </ion-select>
+              </ion-item>
+
+              <ion-card>
+                <ion-card-header>
+                  <ion-card-title>{{ $t("Purchase date") }}</ion-card-title>
+                </ion-card-header>
+                <ion-card-content>
+                  <ion-chip>
+                    <ion-label>PO #</ion-label>
+                  </ion-chip>
+                  <ion-chip>
+                    <ion-label>PO #</ion-label>
+                  </ion-chip>
+                </ion-card-content>
+              </ion-card>
+
+              <ion-item>
+                <ion-label>{{ $t("Size") }}</ion-label>
+                <ion-select value="any">
+                  <ion-select-option value="any">all</ion-select-option>
+                </ion-select>
+              </ion-item>
+              <ion-item>
+                <ion-label>{{ $t("Color") }}</ion-label>
+                <ion-select value="any">
+                  <ion-select-option value="any">all</ion-select-option>
+                </ion-select>
+              </ion-item>
+
+              <ion-card>
+                <ion-card-header>
+                  <ion-card-title>{{ $t("Purchase date") }}</ion-card-title>
+                </ion-card-header>
+                <ion-card-content>
+                  <ion-chip>
+                    <ion-label>PO #</ion-label>
+                  </ion-chip>
+                  <ion-chip>
+                    <ion-label>PO #</ion-label>
+                  </ion-chip>
+                </ion-card-content>
+              </ion-card>
+            </ion-list>
+
+            <ion-list>
+              <ion-list-header>{{ $t("Order") }}</ion-list-header>
+              <ion-item>
+                <ion-label>{{ $t("order created") }}</ion-label>
+                <ion-checkbox />
+              </ion-item>
+              <ion-item>
+                <ion-label>{{ $t("order created") }}</ion-label>
+                <ion-checkbox />
+              </ion-item>
+
+              <ion-card>
+                <ion-card-header>
+                  <ion-card-title>{{ $t("Purchase date") }}</ion-card-title>
+                </ion-card-header>
+                <ion-card-content>
+                  <ion-chip>
+                    <ion-label>PO #</ion-label>
+                  </ion-chip>
+                  <ion-chip>
+                    <ion-label>PO #</ion-label>
+                  </ion-chip>
+                </ion-card-content>
+              </ion-card>
+            </ion-list>
+
+            <ion-list>
+              <ion-list-header>{{ $t("Location") }}</ion-list-header>
+              <ion-item>
+                <ion-label>{{ $t("Product Store") }}</ion-label>
+                <ion-select value="any">
+                  <ion-select-option value="any">Australia</ion-select-option>
+                </ion-select>
+              </ion-item>
+              <ion-item>
+                <ion-label>{{ $t("Facility") }}</ion-label>
+                <ion-select value="any">
+                  <ion-select-option value="any">California Warehouse</ion-select-option>
+                </ion-select>
+              </ion-item>
+           </ion-list>
+        </aside>
+
+        <main class="main">
+          <section class="sort">
+            <ion-item lines="none" class="border-sort">
+              <ion-icon slot="start" :icon="folderOutline" />
+              <ion-label>{{ "Group by" }}</ion-label>
+              <ion-select value="any">
+                <ion-select-option value="any">Partent</ion-select-option>
+              </ion-select>
+            </ion-item>
+            <ion-item lines="none">
+              <ion-icon slot="start" :icon="swapVerticalOutline" />
+              <ion-label>{{ $t("Sort") }}</ion-label>
+              <ion-select value="any">
+                <ion-select-option value="any">{{ $t("Product name") }}</ion-select-option>
+              </ion-select>
+            </ion-item>
+          </section>
+
+          <div class="image-info">
+            <div class="product-image desktop-only">
+                <Image src="https://cdn.shopify.com/s/files/1/0069/7384/9727/products/test-track.jpg?v=1626255137" />
+              </div>
+
+            <section class="section-header">
+            <div class="primary-info">
+              <ion-item>
+                <ion-thumbnail slot="start" class="mobile-only">
+                  <Image src="https://cdn.shopify.com/s/files/1/0069/7384/9727/products/test-track.jpg?v=1626255137" />
+                </ion-thumbnail>
+                <ion-label>
+                  <p>Brand</p>
+                  Virtual Name
+                  <p>{{ $t("Color") }} : color</p>
+                  <p>{{ $t("Size") }}: size</p>
+                </ion-label>
+              </ion-item>
+            </div>
+            <div class="tags desktop-only">
+              <ion-chip>
+                <ion-icon />
+                <ion-label>Shopify ID</ion-label>
+              </ion-chip>
+            </div>
+            <div class="metadata">
+              <ion-item lines="none" detail>
+                <ion-note slot="end" class="metatags">3 variants</ion-note>
+              </ion-item>
+            </div>
+            </section>
+
+          <div class="desktop-only">
+              <ion-list>
+                <ion-list-header>Variants</ion-list-header>
+
+                <div class="virtual-detail">
+                  <div class="virtual-info">
+                    <ion-item lines="none">
+                      <ion-label>
+                        SKU
+                        <p>{{ $t("Color") }} : color</p>
+                        <p>{{ $t("Size") }}: size</p>
+                      </ion-label>
+                    </ion-item>
+                  </div>
+                  <div class="virtual-tags">
+                    <ion-chip>
+                      <ion-icon />
+                      <ion-label>{{ $t("Shopify ID") }}</ion-label>
+                    </ion-chip>
+                  </div>
+                  <div class="virtual-metadata">
+                    <ion-item lines="none" detail>
+                      <ion-note slot="end" class="metatags">3 variants</ion-note>
+                    </ion-item>
+                  </div>
+                </div>
+                <div class="virtual-detail">
+                  <div class="virtual-info">
+                    <ion-item lines="none">
+                      <ion-label>
+                        SKU
+                        <p>{{ $t("Color") }} : color</p>
+                        <p>{{ $t("Size") }}: size</p>
+                      </ion-label>
+                    </ion-item>
+                  </div>
+                  <div class="virtual-tags">
+                    <ion-chip>
+                      <ion-icon />
+                      <ion-label>{{ $t("Shopify ID") }}</ion-label>
+                    </ion-chip>
+                  </div>
+                  <div class="virtual-metadata">
+                    <ion-item lines="none" detail>
+                      <ion-note slot="end" class="metatags">3 variants</ion-note>
+                    </ion-item>
+                  </div>
+                </div>
+              </ion-list>
+            </div>
+          </div>
+        
+          <section class="section-grid">
+          </section>
+        </main>
+       </div>
+
+
+
+      <!-- <div class="product-find">
         <div class="product-search">
           <ion-searchbar />
         </div>
@@ -190,7 +400,7 @@
             </div>
           </div>
         </div>
-      </div>
+      </div> -->
     </ion-content>
   </ion-page>
 </template>
@@ -268,92 +478,37 @@ export default {
   },
 };
 </script>
+
 <style scoped>
-.product-detail {
+
+.image-info {
+  grid:
+      "image detail"
+      / 115px auto;
+}
+
+.product-image {
+  height: 180px;
+  grid-area: image;
+}
+.virtual-detail {
   display: grid;
-  grid: "virtual tags metadata" / max-content 1fr max-content;
+  grid: "info tags metadata" / max-content 1fr max-content;
   grid-area: detail;
+  align-items: center;
   border-bottom: 1px solid #92949c;
 }
 
-.product-search {
-  grid-area: search;
+.virtual-info {
+  grid-area: info;
 }
-.desktop-only {
-  display: none;
-}
-.product-filter {
-  grid-area: filter;
-  display: none;
-}
-.product-sort {
-  grid-area: sort;
-}
-.products {
-  grid-area: products;
-}
-.product {
-  display: grid;
-  grid: "detail";
-  grid-area: product;
-}
-.product-image {
-  grid-area: image;
-  display: none;
-}
-.product-virtual {
-  grid-area: virtual;
-}
-.product-tags {
+
+.virtual-tags {
   grid-area: tags;
-  display: none;
-  align-self: center;
-  justify-content: center;
+  justify-self: center;
 }
-.product-metadata {
+
+.virtual-metadata {
   grid-area: metadata;
-  justify-content: center;
-}
-
-
-@media (min-width: 900px) {
-  .product-find {
-    display: grid;
-    grid:
-      "search sort"
-      "filter products"/ 345px auto;
-  }
-  .product {
-    grid:
-      "image detail"
-      / 115px auto;
-  }
-  .desktop-only {
-    display: unset;
-  }
-  .product-tags {
-    display: unset;
-    display: flex;
-  }
-  .product-image {
-    display: unset;
-    height: 180px;
-  }
-  .mobileonly-image {
-    display: none;
-  }
-
-  .product-sort {
-    display: flex;
-    justify-content: flex-end;
-  }
-  ion-select {
-    max-width: 100%;
-  }
-  .border-sort {
-    border-right: 0.5px solid #92949c;
-    border-left: 0.5px solid #92949c;
-    height: 48px;
-  }
 }
 </style>
