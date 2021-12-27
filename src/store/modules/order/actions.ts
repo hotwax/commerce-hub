73abe9c70c57df9f58b47db9630dcdc25e3e16e7
Aniket Kumar Pandey <meet.aniket.kumar@gmail.com>
2021-12-27 18:06:30 +0530

    Removed unnecessary code which was commented (#1x634cr)

--- src/store/modules/order/actions.ts
+++ src/store/modules/order/actions.ts
@@ -44,29 +44,3 @@ const actions: ActionTree<OrderState, RootState> = {
 } 
 
 export default actions
-
-
-// resp = await ProductService.fetchProducts({
-//   // used sku as we are currently only using sku to search for the product
-//   "filters": ['sku: ' + payload.queryString],
-//   "viewSize": payload.viewSize,
-//   "viewIndex": payload.viewIndex
-// })
-
-// // resp.data.response.numFound tells the number of items in the response
-// if (resp.status === 200 && resp.data.response.numFound > 0 && !hasError(resp)) {
-//   let products = resp.data.response.docs;
-//   const totalProductsCount = resp.data.response.numFound;
-
-//   if (payload.viewIndex && payload.viewIndex > 0) products = state.products.list.concat(products)
-//   commit(types.PRODUCT_SEARCH_UPDATED, { products: products, totalProductsCount: totalProductsCount })
-// } else {
-//   //showing error whenever getting no products in the response or having any other error
-//   showToast(translate("Product not found"));
-// }
-// // Remove added loader only when new query and not the infinite scroll
-// if (payload.viewIndex === 0) emitter.emit("dismissLoader");
-//     } catch (error) {
-//   console.log(error)
-//   showToast(translate("Something went wrong"));
-// }
