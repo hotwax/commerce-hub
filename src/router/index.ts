ec31befba459da9d80db39e736ad5267695a7c3e
Azkya Khan <azkya.khan@hotwax.co>
2022-01-04 17:32:47 +0530

    Improved pages name and their routes (#1xqg7ne)

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,14 @@
 
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import Home from '@/views/Home.vue'
-import Order from '@/views/Order.vue'
-import OrderFind from '@/views/OrderFind.vue'
-import ProductInventory from '@/views/ProductInventory.vue'
-import PurchaseOrder from '@/views/PurchaseOrder.vue'
-import ProductInventoryDetail from '@/views/ProductInventoryDetail.vue'
-import PurchaseOrderDetail from '@/views/PurchaseOrderDetail.vue'
 import Login from '@/views/Login.vue'
+import Home from '@/views/Home.vue'
+import FindOrder from '@/views/FindOrder.vue'
+import Order from '@/views/Order.vue'
+import FindProductInventory from '@/views/FindProductInventory.vue'
+import ProductInventory from '@/views/ProductInventory.vue'
+import FindPurchaseOrder from '@/views/FindPurchaseOrder.vue'
+import PurchaseOrder from '@/views/PurchaseOrder.vue'
 import Settings from "@/views/Settings.vue"
 import store from '@/store'
 
@@ -31,41 +31,13 @@ const loginGuard = (to: any, from: any, next: any) => {
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/purchase-order-detail'
+    redirect: '/home'
   },
   {
-    path: '/order',
-    name: 'Order',
-    component: Order,
-    beforeEnter: authGuard
-  },
-  {
-    path: '/orderfind',
-    name: 'OrderFind',
-    component: OrderFind,
-    beforeEnter: authGuard
-  },
-  {
-    path: '/product',
-    name: 'ProductInventory',
-    component: ProductInventory,
-    beforeEnter: authGuard
-  },
-  {
-    path: '/purchase-order',
-    name: 'PurchaseOrder',
-    component:PurchaseOrder,
-  },
-  {
-    path: '/product-inventory-detail',
-    name: 'ProductInventoryDetail',
-    component: ProductInventoryDetail,
-  },
-  {  
-    path: '/purchase-order-detail',
-    name: 'PurchaseOrderDetail',
-    component: PurchaseOrderDetail,
-    beforeEnter: authGuard
+    path: '/login',
+    name: 'Login',
+    component: Login,
+    beforeEnter: loginGuard
   },
   {
     path: '/home',
@@ -74,11 +46,39 @@ const routes: Array<RouteRecordRaw> = [
     beforeEnter: authGuard
   },
   {
-    path: '/login',
-    name: 'Login',
-    component: Login,
-    beforeEnter: loginGuard
+    path: '/find-order',
+    name: 'FindOrder',
+    component: FindOrder,
+    beforeEnter: authGuard
   },
+  {
+    path: '/order',
+    name: 'Order',
+    component: Order,
+    beforeEnter: authGuard
+  },
+  {
+    path: '/find-product-inventory',
+    name: 'FindProductInventory',
+    component:FindProductInventory,
+    beforeEnter: authGuard
+  },
+  {
+    path: '/product-inventory',
+    name: 'ProductInventory',
+    component: ProductInventory,
+  },
+  {
+    path: '/find-purchase-order',
+    name: 'FindPurchaseOrder',
+    component:FindPurchaseOrder,
+  },
+  {  
+    path: '/purchase-order',
+    name: 'PurchaseOrder',
+    component: PurchaseOrder,
+    beforeEnter: authGuard
+  },  
   {
     path: "/settings",
     name: "Settings",
---
+++ src/views/FindOrder.vue
@@ -0,0 +1,332 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-back-button default-href="/" slot="start" />
+        <ion-title>{{ $t("Orders") }}</ion-title>
+        <ion-buttons slot="end">
+          <ion-button fill="clear">
+            <ion-icon slot="icon-only" :icon="syncOutline" />
+          </ion-button>
+          <ion-button fill="clear">
+            <ion-icon slot="icon-only" :icon="downloadOutline" />
+          </ion-button>
+          <ion-button fill="clear" class="mobile-only">
+            <ion-icon slot="icon-only" :icon="filterOutline" />
+          </ion-button>
+        </ion-buttons>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content>
+      <div class="find">
+        <section class="search">
+          <ion-searchbar />
+        </section>
+
+        <aside class="filters desktop-only">
+          <ion-list>
+            <ion-list-header>Date</ion-list-header>
+            <ion-item>
+              <ion-label>order created</ion-label>
+              <ion-select value="any">
+                <ion-select-option value="any">any</ion-select-option>
+              </ion-select>
+            </ion-item>
+            <ion-item>
+              <ion-label>order created</ion-label>
+              <ion-select value="any">
+                <ion-select-option value="any">any</ion-select-option>
+              </ion-select>
+            </ion-item>
+            <ion-item>
+              <ion-label>order created</ion-label>
+              <ion-select value="any">
+                <ion-select-option value="any">any</ion-select-option>
+              </ion-select>
+            </ion-item>
+          </ion-list>
+          <ion-list>
+            <ion-list-header>Type</ion-list-header>
+            <ion-item>
+              <ion-label>order created</ion-label>
+              <ion-checkbox />
+            </ion-item>
+            <ion-item>
+              <ion-label>order created</ion-label>
+              <ion-checkbox />
+            </ion-item>
+            <ion-item>
+              <ion-label>order created</ion-label>
+              <ion-checkbox />
+            </ion-item>
+          </ion-list>
+          <ion-list>
+            <ion-list-header>Date</ion-list-header>
+            <ion-item>
+              <ion-label>order created</ion-label>
+              <ion-select value="any">
+                <ion-select-option value="any">any</ion-select-option>
+              </ion-select>
+            </ion-item>
+            <ion-item>
+              <ion-label>order created</ion-label>
+              <ion-select value="any">
+                <ion-select-option value="any">any</ion-select-option>
+              </ion-select>
+            </ion-item>
+            <ion-item>
+              <ion-label>order created</ion-label>
+              <ion-select value="any">
+                <ion-select-option value="any">any</ion-select-option>
+              </ion-select>
+            </ion-item>
+          </ion-list>
+
+          <ion-card>
+            <ion-toolbar>
+              <ion-title>Purchase date</ion-title>
+            </ion-toolbar>
+            <ion-card-content>
+              <ion-chip>
+                <ion-label>PO #</ion-label>
+              </ion-chip>
+              <ion-chip>
+                <ion-label>PO #</ion-label>
+              </ion-chip>
+            </ion-card-content>
+          </ion-card>
+        </aside>
+
+        <main class="main">
+          <section class="sort"></section>
+
+           <hr />
+
+          <section class="section-header">
+            <div class="primary-info">
+               <ion-item lines="none">
+                <ion-label>
+                  Order ID
+                  <p>Customer Name</p>
+                </ion-label>
+              </ion-item>
+            </div>
+
+            <div class="tags">
+              <ion-chip>
+                <ion-icon :icon="pricetag" />
+                <ion-label>Shopify ID</ion-label>
+              </ion-chip>
+              <ion-chip>
+                <ion-icon :icon="pricetag" />
+                <ion-label>Customer Loyalty Status</ion-label>
+              </ion-chip>
+            </div>
+
+            <div class="metadata">
+              <ion-note>Ordered on 7 Jan 2021</ion-note>
+              <ion-badge>Approved</ion-badge>
+            </div>
+          </section>
+
+          <section class="section-grid">
+            <ion-card @click="() => router.push('/order')">
+              <ion-item>
+                <ion-thumbnail slot="start">
+                  <Image />
+                </ion-thumbnail>
+                <ion-label>
+                  <p>Brand</p>
+                  Virtual Name
+                  <p>{{ $t("Color") }} : color</p>
+                  <p>{{ $t("Size") }}: size</p>
+                </ion-label>
+                <ion-badge color="primary" slot="end">Approved</ion-badge>
+              </ion-item>
+              <ion-item>
+                <ion-label> {{ $t("Promise date") }} </ion-label>
+                <p slot="end">14 Jan 2021</p>
+              </ion-item>
+              <ion-item>
+                <ion-label> {{ $t("PO arrival date") }} </ion-label>
+                <p slot="end">14 Jan 2021</p>
+              </ion-item>
+              <ion-item>
+                <ion-label> {{ $t("Last brokered") }} </ion-label>
+                <p slot="end">California Warehouse</p>
+              </ion-item>
+            </ion-card>
+            <ion-card>
+              <ion-item>
+                <ion-thumbnail slot="start">
+                  <Image />
+                </ion-thumbnail>
+                <ion-label>
+                  <p>Brand</p>
+                  Virtual Name
+                  <p>{{ $t("Color") }} : color</p>
+                  <p>{{ $t("Size") }}: size</p>
+                </ion-label>
+                <ion-badge color="primary" slot="end">Approved</ion-badge>
+              </ion-item>
+              <ion-item>
+                <ion-label> {{ $t("Promise date") }} </ion-label>
+                <p slot="end">14 Jan 2021</p>
+              </ion-item>
+              <ion-item>
+                <ion-label> {{ $t("PO arrival date") }} </ion-label>
+                <p slot="end">14 Jan 2021</p>
+              </ion-item>
+              <ion-item>
+                <ion-label> {{ $t("Last brokered") }} </ion-label>
+                <p slot="end">California Warehouse</p>
+              </ion-item>
+            </ion-card>
+            <ion-card>
+              <ion-item>
+                <ion-thumbnail slot="start">
+                  <Image />
+                </ion-thumbnail>
+                <ion-label>
+                  <p>Brand</p>
+                  Virtual Name
+                  <p>{{ $t("Color") }} : color</p>
+                  <p>{{ $t("Size") }}: size</p>
+                </ion-label>
+                <ion-badge color="primary" slot="end">Approved</ion-badge>
+              </ion-item>
+              <ion-item>
+                <ion-label> {{ $t("Promise date") }} </ion-label>
+                <p slot="end">14 Jan 2021</p>
+              </ion-item>
+              <ion-item>
+                <ion-label> {{ $t("PO arrival date") }} </ion-label>
+                <p slot="end">14 Jan 2021</p>
+              </ion-item>
+              <ion-item>
+                <ion-label> {{ $t("Last brokered") }} </ion-label>
+                <p slot="end">California Warehouse</p>
+              </ion-item>
+            </ion-card>
+            <ion-card>
+              <ion-item>
+                <ion-thumbnail slot="start">
+                  <Image />
+                </ion-thumbnail>
+                <ion-label>
+                  <p>Brand</p>
+                  Virtual Name
+                  <p>{{ $t("Color") }} : color</p>
+                  <p>{{ $t("Size") }}: size</p>
+                </ion-label>
+                <ion-badge color="primary" slot="end">Approved</ion-badge>
+              </ion-item>
+              <ion-item>
+                <ion-label> {{ $t("Promise date") }} </ion-label>
+                <p slot="end">14 Jan 2021</p>
+              </ion-item>
+              <ion-item>
+                <ion-label> {{ $t("PO arrival date") }} </ion-label>
+                <p slot="end">14 Jan 2021</p>
+              </ion-item>
+              <ion-item>
+                <ion-label> {{ $t("Last brokered") }} </ion-label>
+                <p slot="end">California Warehouse</p>
+              </ion-item>
+            </ion-card>
+          </section>
+
+          <hr />
+        </main>
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import Image from "../components/Image.vue";
+import {
+  IonBackButton,
+  IonBadge,
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCheckbox,
+  IonChip,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonNote,
+  IonPage,
+  IonSearchbar,
+  IonSelect,
+  IonThumbnail,
+  IonTitle,
+  IonToolbar,
+  IonSelectOption,
+} from "@ionic/vue";
+import { pricetag, ribbon, downloadOutline, syncOutline, filterOutline } from "ionicons/icons";
+import { useRouter } from 'vue-router';
+
+export default {
+  name: "Order",
+  components: {
+    Image,
+    IonSelectOption,
+    IonBackButton,
+    IonBadge,
+    IonButton,
+    IonCard,
+    IonCardContent,
+    IonCheckbox,
+    IonChip,
+    IonContent,
+    IonHeader,
+    IonIcon,
+    IonItem,
+    IonLabel,
+    IonList,
+    IonListHeader,
+    IonNote,
+    IonPage,
+    IonSearchbar,
+    IonSelect,
+    IonThumbnail,
+    IonTitle,
+    IonToolbar,
+  },
+  setup() {
+    const router = useRouter();
+
+    return {
+      pricetag,
+      ribbon,
+      downloadOutline,
+      syncOutline,
+      filterOutline,
+      router
+    };
+  },
+};
+</script>
+
+<style scoped>
+.metadata {
+  text-align: end;
+}
+
+.metadata > ion-note {
+  display: block;
+}
+
+hr {
+  border-width: 1px;
+  color: var(--ion-color-medium);
+}
+</style>
+
