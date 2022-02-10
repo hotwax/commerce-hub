eddeffbafe9dd4bfcc339e48e2515593dfd57959
Azkya Khan <azkya.khan@hotwax.co>
2022-02-10 12:11:52 +0530

    Added space between individual items of internal navigation in mobile view (#1zhjrv3)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -157,14 +157,10 @@ export default defineComponent({
   height: 200px;
 }
 
- .internal {
-   margin: var(--spacer-sm) var( --spacer-xs) 0 var( --spacer-xs);
- }
-
 .internal > ion-item {
   border: 1px solid var(--ion-color-medium);
   border-radius: 15px;
-  margin-bottom: var(--spacer-xs);
+  margin: var(--spacer-xs) var( --spacer-xs) 0;
 }
 
 ion-card > ion-card-header {
@@ -207,12 +203,8 @@ ion-card > ion-card-header {
     display: flex;
     flex-direction: column;
     justify-content: space-between;
-    margin-top: unset;
+    margin: unset;
  }
-
- .internal > ion-item {
-   margin-bottom: unset;
-}
 
   main {
     --page-width: 1040px;
