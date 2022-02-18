711b732c921faf84dcecfb22f1dfbf31436a31df
Azkya Khan <azkya.khan@hotwax.co>
2022-02-18 11:52:33 +0530

    Removed splitpane menu (#202re98)

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,20 @@
 <template>
-  <ion-app> 
-    <IonSplitPane content-id="main-content" when="lg">
-      <Menu v-show="showMenu" />
-      <ion-router-outlet id="main-content" />
-    </IonSplitPane>
+  <ion-app>
+    <ion-router-outlet />
   </ion-app>
 </template>
 
 <script lang="ts">
-import { IonApp, IonRouterOutlet, IonSplitPane } from '@ionic/vue';
+import { IonApp, IonRouterOutlet } from '@ionic/vue';
 import { defineComponent } from 'vue';
 import { loadingController } from '@ionic/vue';
 import emitter from "@/event-bus"
-import Menu from '@/components/Menu.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
     IonApp,
-    IonRouterOutlet,
-    IonSplitPane,
-    Menu
-  },
-  computed: {
-    showMenu() {
-      return this.$route.name === 'Settings'
-    }
+    IonRouterOutlet
   },
   data() {
     return {
