1d597290a471e10d3043bf237a08cdda682c90df
azkyakhan <azkya.khan@hotwax.co>
2022-02-25 11:41:52 +0530

    Improved markup and styling (#202regg)

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -2,62 +2,66 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
+        <ion-back-button default-href="/" slot="start" />
         <ion-title>{{ $t("Profile") }}</ion-title>
       </ion-toolbar>
     </ion-header>
 
-    <ion-content :fullscreen="true">
-      <div>
-        <div class="user">
-          <ion-avatar>
-            <Image />
-          </ion-avatar>
+    <ion-content>
+      <main>
+        <div>
+          <ion-item lines="none">
+            <ion-avatar>
+              <Image />
+            </ion-avatar>
+          </ion-item>
 
-          <ion-list>
-            <ion-item>
-              <ion-label position="fixed">{{ $t("First name") }}</ion-label>
-              <ion-input clear-input></ion-input>
-            </ion-item>
-            <ion-item>
-              <ion-label position="fixed">{{ $t("Last name") }}</ion-label>
-              <ion-input clear-input></ion-input>
-            </ion-item>
-            <ion-item lines="none">
-              <ion-label>{{ $t("Role") }}</ion-label>
-              <ion-label slot="end">Admin</ion-label>
-            </ion-item>
-          </ion-list>
+          <ion-item>
+            <ion-label position="fixed">{{ $t("First name") }}</ion-label>
+            <ion-input clear-input></ion-input>
+          </ion-item>
+          <ion-item>
+            <ion-label position="fixed">{{ $t("Last name") }}</ion-label>
+            <ion-input clear-input></ion-input>
+          </ion-item>
+          <ion-item lines="none">
+            <ion-label>{{ $t("Role") }}</ion-label>
+            <ion-label slot="end">Admin</ion-label>
+          </ion-item>
         </div>
 
-        <div class="settings">
-          <ion-label>{{ $t("Settings") }}</ion-label>
-          <ion-list>
-            <ion-item>
-              <ion-label position="fixed">{{ $t("Login") }}</ion-label>
-              <ion-input :placeholder="$t('username')" clear-input></ion-input>
-            </ion-item>
-            <ion-item>
-              <ion-label position="fixed">{{ $t("Password") }}</ion-label>
-              <ion-input :placeholder="$t('password')" type="password" clear-input></ion-input>
-            </ion-item>
-            <ion-item lines="none">
-              <ion-label>{{ $t("Time zone") }}</ion-label>
-              <ion-select value="n">
-                <ion-select-option value="n">New York</ion-select-option>
-                <ion-select-option value="t">Texas</ion-select-option>
-              </ion-select>
-            </ion-item>
-          </ion-list>
+        <div>
+          <ion-item lines="none">
+            <ion-label>{{ $t("Settings") }}</ion-label>
+          </ion-item>
+
+          <ion-item>
+            <ion-label position="fixed">{{ $t("Login") }}</ion-label>
+            <ion-input :placeholder="$t('username')" clear-input></ion-input>
+          </ion-item>
+          <ion-item>
+            <ion-label position="fixed">{{ $t("Password") }}</ion-label>
+            <ion-input :placeholder="$t('password')" type="password" clear-input></ion-input>
+          </ion-item>
+          <ion-item lines="none">
+            <ion-label>{{ $t("Time zone") }}</ion-label>
+            <ion-select value="n">
+              <ion-select-option value="n">New York</ion-select-option>
+              <ion-select-option value="t">Texas</ion-select-option>
+            </ion-select>
+          </ion-item>
         </div>
+
         <ion-button expand="block" color="medium" fill="outline">{{ $t("Logout") }}</ion-button>
-      </div>
+      </main>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
 import {
-  IonAvatar,  
+  IonAvatar,
+  IonBackButton,
   IonButton,
   IonContent,
   IonHeader,
@@ -69,7 +73,7 @@ import {
   IonSelect,
   IonSelectOption,
   IonToolbar,
-  IonTitle
+  IonTitle,
 } from '@ionic/vue';
 import { defineComponent } from 'vue';
 import Image from '@/components/Image.vue';
@@ -77,32 +81,32 @@ import Image from '@/components/Image.vue';
 export default defineComponent({
   name: 'Profile',
   components: {
-    IonAvatar,  
+    IonAvatar,
+    IonBackButton,
     IonButton,
     IonContent,
     IonHeader,
     IonInput,
     IonItem,
     IonLabel,
-    IonList,
     IonPage,
     IonSelect,
     IonSelectOption,
     IonToolbar,
     IonTitle,
-    Image
+    Image,
   },
 });
 </script>
 
 <style scoped>
-ion-content > div {
+main {
   max-width: 375px;
-  margin: var(--spacer-base) auto 0;
+  margin: 0 auto 0;
 }
 
-.user,
-.settings {
-  display: flex;
+main > div,
+ion-button {
+  margin-top: var(--spacer-xl);
 }
 </style>
