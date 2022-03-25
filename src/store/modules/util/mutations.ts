0dade65553ac4c43b37fd8322ae94e960102f5a8
Aniket Kumar Pandey <meet.aniket.kumar@gmail.com>
2022-03-25 11:36:01 +0530

    Implemented: mutation to set facility locations in store (#245e5qn).

--- src/store/modules/util/mutations.ts
+++ src/store/modules/util/mutations.ts
@@ -5,6 +5,10 @@ import UtilState from './UtilState'
 const mutations: MutationTree <UtilState> = {
     [types.UTIL_SHIPMENT_METHODS_UPDATED](state, payload) {
         state.shipmentMethod = payload
+    },
+    [types.UTIL_FACILITY_LOCATIONS_UPDATED](state, payload) {
+        state.facilityLocations = payload
     }
+
 }
 export default mutations;
