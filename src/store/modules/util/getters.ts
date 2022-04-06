262a9c003003af5450f4b12058026eaececcd7dc
Disha Talreja <dishatalreja1202@gmail.com>
2022-04-06 18:54:18 +0530

    Reverted unwanted changes(#1x695h8)

--- src/store/modules/util/getters.ts
+++ src/store/modules/util/getters.ts
@@ -4,12 +4,10 @@ import UtilState from './UtilState';
 
 const getters: GetterTree <UtilState, RootState> = {
     getShipmentMethod: (state) => (shipmentMethodTypeId: string) => {
-        console.log(state.shipmentMethod)
-        // return state.shipmentMethod.find((data: any) => data.shipmentMethodTypeId === shipmentMethodTypeId)?.description
+        return state.shipmentMethod.find((data: any) => data.shipmentMethodTypeId === shipmentMethodTypeId)?.description
     },
     getFacilityName: (state) => (facilityId: any) => {
         return state.facilityList.find((facility: any) => facility.facilityId === facilityId)
-        // return true;
     }
 }
 export default getters;
