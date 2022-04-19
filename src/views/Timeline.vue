5322b8e6dc23f8701918501fde5e9bebe2b1daec
Azkya Khan <azkya.khan@hotwax.co>
2022-04-19 16:42:20 +0530

    Implemented static code for timeline page(#27xkt1t)

---
+++ src/views/Timeline.vue
@@ -0,0 +1,62 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-back-button default-href="/" slot="start" />
+        <ion-title>{{ $t("Timeline") }}</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content>
+      <ion-list>
+        <ion-item v-for="item in 5" :key="item">
+        <ion-icon :icon="ticketOutline" slot="start" />
+        <ion-label>
+          <p>+10 minutes</p>
+          Imported from Shopify
+        </ion-label>
+        <ion-icon slot="end" :icon="informationCircleOutline" />
+        </ion-item>
+      </ion-list>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import {
+  IonBackButton,
+  IonContent,
+  IonHeader,
+  IonItem,
+  IonIcon,
+  IonLabel,
+  IonList,
+  IonPage,
+  IonToolbar,
+  IonTitle,
+} from '@ionic/vue';
+import { defineComponent } from 'vue';
+import { informationCircleOutline, ticketOutline } from 'ionicons/icons'
+
+export default defineComponent({
+  name: 'Timeline',
+  components: {
+    IonBackButton,
+    IonContent,
+    IonHeader,
+    IonItem,
+    IonIcon,
+    IonLabel,
+    IonList,
+    IonPage,
+    IonToolbar,
+    IonTitle,
+  },
+  setup() {
+    return {
+      informationCircleOutline,
+      ticketOutline
+    }
+  }
+});
+</script>
