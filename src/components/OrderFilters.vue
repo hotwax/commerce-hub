36f352f2cd9f19860ee66a85dabfc2fe2b4e2d16
Yash Maheshwari <yashmaheshwari551@gmail.com>
2022-04-22 18:03:22 +0530

    Improved: code to define service for fetching the product store info(#28ejub5)

--- src/components/OrderFilters.vue
+++ src/components/OrderFilters.vue
@@ -64,6 +64,12 @@
         <ion-select-option value="WAREHOUSE" >{{ $t('Warehouse') }}</ion-select-option>
       </ion-select>
     </ion-item>
+    <ion-item>
+      <ion-label>{{ $t("Product Store") }}</ion-label>
+      <ion-select :value="query.productStoreId" @ionChange="updateAppliedFilters($event['detail'].value, 'productStoreId')" interface="popover">
+        <ion-select-option v-for="[id, storeName] in Object.entries(getEComStores)" :key="id" :value="id">{{ storeName }}</ion-select-option>
+      </ion-select>
+    </ion-item>
   </ion-list>
 
   <ion-card>
@@ -121,7 +127,8 @@ export default defineComponent({
   computed: {
     ...mapGetters({
       query: 'order/getOrderQuery',
-      getShipmentMethodDesc: 'util/getShipmentMethod'
+      getShipmentMethodDesc: 'util/getShipmentMethod',
+      getEComStores: 'util/getEcomStores'
     })
   },
   methods: {
